a672ce63e0bef4373c2c4237a9016cab247efff6
shreelock <shreenath.dutt@gmail.com>
2015-07-12 19:17:45 +0530

    Segregated messages and notifs, extracted people and timestamps

--- proc1.py
+++ proc1.py
@@ -1,17 +1,45 @@
 import re
+month_dict={'Jan':1,'Feb':2,'Mar':3,'Apr':4,'May':5,'Jun':6,'Jul':7,'Aug':8,'Sep':9,'Oct':10,'Nov':11,'Dec':12};
+
+def get_timestamp(str):
+	Obj = re.search(r'(.*):(.*), (.*?) (.*)',str);
+	hh=Obj.group(1).strip();
+	mm=Obj.group(2).strip();
+	dd=Obj.group(3).strip();
+	mon=Obj.group(4).strip();
+	timestamp=month_dict[mon]*1000000+int(dd)*10000+int(hh)*100+int(mm);
+	return timestamp;
+
+
 f=open('../wachat.txt')
 chat_db=[]
 entry=[]
-#next=f.readline()
-for next in f.readlines():#!= " ":
-	print next
+for next in f.readlines():
+	#print next.strip();
 	try:
-		#Matching with the regular expression of the text line;
-		searchObj = re.search(r'(.*):(.*), (.*?) (.*?) (.*)',next);
-		print searchObj.group(1)
-		print searchObj.group(2)
-		print searchObj.group(3)
-		print searchObj.group(4)
-		print searchObj.group(5)
-	except AttributeError:
-		pass
+		if(next[2]==':')&(next[5]==','):
+			index1 = next.index('-');
+			timestring = next[0:index1];
+			timestamp=get_timestamp(timestring);
+			#print timestamp;
+			datastring = next[index1+1:].strip();
+			#print datastring;
+			if((datastring.find('Shreenath:')!=-1) \
+			 | (datastring.find('Tanmay Verma:')!=-1) \
+			 | (datastring.find('Pratik Kumar:')!=-1) \
+			 | (datastring.find('Ankita Kalra:')!=-1) \
+			 | (datastring.find('Mohan Krishna:')!=-1) ):
+			#for segregating between chat and Notifs
+				#Matching with the regular expression of the text line;
+				sObj = re.search(r'(.*): (.*)',datastring);
+				print "1 = ",sObj.group(1)
+				print "2 = ",sObj.group(2)
+			else:
+				print datastring;
+
+			#except AttributeError: 	#The string did not match the RegEx
+			#	pass				#It was probably a group notification
+		else:
+			print next;
+	except IndexError: #The string was not a new dialogue, but it was
+			pass;	   #continuation of an old one.
