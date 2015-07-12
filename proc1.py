d4dc8c762d846c907aa21878e18485801f4c8655
shreelock <shreenath.dutt@gmail.com>
2015-07-12 16:11:32 +0530

    Generated the RegEx for a line

--- proc1.py
+++ proc1.py
@@ -1,15 +1,17 @@
 import re
-f=open('wachat.txt')
+f=open('../wachat.txt')
 chat_db=[]
 entry=[]
-next=f.readline()
-index1 = next.index('-');
-print index1
-timestamp = next[0:index1];
-print timestamp
-
-#while next!=" ":
-#	print next
-#	next=f.readline()
-#for month in f.readlines():
-#	print(month.strip())
+#next=f.readline()
+for next in f.readlines():#!= " ":
+	print next
+	try:
+		#Matching with the regular expression of the text line;
+		searchObj = re.search(r'(.*):(.*), (.*?) (.*?) (.*)',next);
+		print searchObj.group(1)
+		print searchObj.group(2)
+		print searchObj.group(3)
+		print searchObj.group(4)
+		print searchObj.group(5)
+	except AttributeError:
+		pass
